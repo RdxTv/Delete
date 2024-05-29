eae162f816aae38817e44213b27498fe10082cd2
DesiXCloud <164381773+DesiXCloud@users.noreply.github.com>
2024-05-29 23:13:57 +0530

    Update insta.py

--- plugins/helper/insta.py
+++ plugins/helper/insta.py
@@ -23,9 +23,9 @@ async def link_handler(Mbot, message):
         url= link.replace("instagram.com","ddinstagram.com")
         url=url.replace("==","%3D%3D")
         if url.endswith("="):
-           dump_file=await message.reply_video(url[:-1],caption="Downloaded By @DesixBot")
+           dump_file=await message.reply_video(url[:-1],caption="Downloaded By @UncutFlixBot")
         else:
-            dump_file=await message.reply_video(url,caption="Downloaded By @DesixBot")
+            dump_file=await message.reply_video(url,caption="Downloaded By @UncutFlixBot")
         if 'dump_file' in locals():
            await dump_file.forward(DUMP_GROUP)
         await m.delete()
@@ -54,9 +54,9 @@ async def link_handler(Mbot, message):
                       return await message.reply("oops something went wrong")
                try:
                    if ddinsta:
-                      dump_file=await message.reply_video(content_value,caption="Downloaded By @DesixBot")
+                      dump_file=await message.reply_video(content_value,caption="Downloaded By @UncutFlixBot")
                    else:
-                       dump_file=await message.reply_video(content_value, caption="Downloaded By @DesixBot")
+                       dump_file=await message.reply_video(content_value, caption="Downloaded By @UncutFlixBot")
                except:
                    downfile=f"{os.getcwd()}/{random.randint(1,10000000)}"
                    with open(downfile,'wb') as x:
@@ -75,7 +75,7 @@ async def link_handler(Mbot, message):
                      com=await message.reply_text(meta[i])
                      await asyncio.sleep(1)
                      try:
-                        dump_file=await message.reply_video(com.text,caption="Downloaded By @DesixBot")
+                        dump_file=await message.reply_video(com.text,caption="Downloaded By @UncutFlixBot")
                         await com.delete()
                      except:
                          pass 
@@ -92,7 +92,7 @@ async def link_handler(Mbot, message):
                       com=await message.reply(meta[0])
                       await asyncio.sleep(1)
                       try:
-                          dump_file=await message.reply_video(com.text,caption="Downloaded By @DesixBot")
+                          dump_file=await message.reply_video(com.text,caption="Downloaded By @UncutFlixBot")
                           await com.delete()
                       except:
                           pass
